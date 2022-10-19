7183fb6a486dbaf8d1c35bbab48dfaa822f7a6e6
Felix Ouma <fatonoh@gmail.com>
2022-10-19 20:57:42 +0300

    Update available Api greetings

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_221_019_084_844) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_084844) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'greetings', force: :cascade do |t|
-    t.string 'greeting'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "greetings", force: :cascade do |t|
+    t.string "greeting"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
 end
